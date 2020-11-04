96ee3e47cb3137fe5103676452b2e33471ffaff5
PauloEduardoLucas <dudu.magnificat@gmail.com>
2020-11-04 20:22:20 -0300

    Updating more formatting ways to data

--- datas.js
+++ datas.js
@@ -1,8 +1,7 @@
 let now = new Date();
 
-dayWeek = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
-dayMonth = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
-
+dayWeek = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado");
+dayMonth = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro");
 
 //Pega tudo
 console.log("Tudo:", now);
@@ -14,7 +13,7 @@ console.log("Dia da semana:", dayWeek[now.getDay()]);
 console.log("Dia:",now.getDate());
 
 //Pega o mes como inteiro
-console.log("Mês:",dayMonth[now.getMonth()] )
+console.log("Mês:",dayMonth[now.getMonth()]);
 
 //Pega o ano
 console.log("Ano:",now.getFullYear());
@@ -40,11 +39,20 @@ console.log("Hora formatada:",now.toLocaleTimeString());
 console.log("Padrão(USA):",now.toLocaleDateString());
 
 //Data formatada na mão 
-console.log(`Na mão1(BR): ${now.getDate()}-${now.getMonth()+1}-${now.getFullYear()}`)
-console.log(`Na mão2(BR): ${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`)
+console.log(`Na mão1(BR): ${now.getDate()}-${now.getMonth()+1}-${now.getFullYear()}`);
+console.log(`Na mão2(BR): ${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`);
 
 //Nao entendi muito bem mas funciona
 console.log("Stack Overflow:", now.toISOString().substr(0, 10).split('-').reverse().join('/'));
 
 //Hora padrao
-console.log("Hora formatada:",now.toLocaleTimeString(0))
+console.log("Hora formatada:",now.toLocaleTimeString(0));
+console.log(now.toISOString());
+console.log(now.toISOString().substr(0, 10).split('-').reverse().join('/'));
+console.log(now.toISOString().substr(11, 8));
+
+
+
+let cicero = "1234567891"
+
+console.log(cicero.substr(2, 6));
